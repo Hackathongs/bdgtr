0c64fcd7d55d865b2e0ac42f7e34ffea21de45a7
Marvin Corro <corro.marvin@gmail.com>
2017-09-04 12:43:48 -0700

    quick update

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Header from './Header';
 
 export default class App extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ export default class App extends Component {
     return (
       <div>
         <p>Hello, Reacts! HI STEVEN!</p>
+        <Header />
       </div>
     );
   }
---
+++ src/client/app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Header extends Component{
+    render(){
+        return (
+            <div>
+                This is your footer
+            </div>
+        );
+    }
+}
+export default Header;
